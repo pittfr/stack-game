970b2034ac2d603bc3a75a6f2583b05fd8463363
Pitt <dpittmor@gmail.com>
2025-02-15 09:50:07 +0000

    Update stack.py
    fixed tower colors and drawing

--- stack.py
+++ stack.py
@@ -1,4 +1,5 @@
 import pygame
+import numpy as np
 import random
 
 pygame.init()
@@ -12,40 +13,64 @@ screen = pygame.display.set_mode(windowRes)
 pygame.display.set_caption("Stack!")
 
 PHEIGHT = 2.5 #platform height
-ISO_MULTIPLIER = 10
-
+ISO_MULTIPLIER = 20
 
 class Platform:
     #the height of the cube is always the same, the only thing that changes is the base's dimensions
-    def __init__(self, width, depth, rgb, moving): #moving can either be true or false (part of the tower)
+    def __init__(self, width, depth, moving, z_offset): #moving can either be true or false (false means the platform is a part of the tower)
         self.moving = moving
 
         self.width = width
         self.depth = depth
         self.height = PHEIGHT
+        self.z_offset = z_offset
 
         self.leaningFactor = 0.9
 
+        self.colors = None
+        self.vertices = None
+        self.edges = None
+        self.faces = None
+        
+    def setup(self, rgb):
         self.colors = self.defineColors(rgb)
         self.vertices = self.defineVertices()
         self.edges = self.defineVisibleEdges()
         self.faces = self.defineFaces()
+    
+    def lightenColor(self, rgb, factor=1.2):
+        return tuple(max(0, min(255, int(c * factor))) for c in rgb)
 
     def defineColors(self, rgb):
-        if self.moving:
-            def lightenColor(rgb, factor=1.2):
-                return tuple(min(255, int(c * factor)) for c in rgb)
-            return [lightenColor(rgb, 1.3), lightenColor(rgb, .7), rgb]
-        else:
-            return rgb
+        return [self.lightenColor(rgb, 1.4), self.lightenColor(rgb, .6), rgb]
 
     def defineVertices(self):
-        vertices = [(x, y, z)
-                    for x in (0, self.width)
-                    for y in (0, self.depth)
-                    for z in (0, self.height)]
-        
+        #defining the base grid
+        x = np.array([0, self.width])
+        y = np.array([0, self.depth])
+        z = np.array([0, self.height])
+
+        #generating the actual grid
+        X, Y, Z = np.meshgrid(x, y, z, indexing="ij")
+
+        vertices = np.column_stack([X.ravel(), Y.ravel(), Z.ravel()])
+
+        if(not self.moving):
+            vertices[:, 2] += self.z_offset
+
         return vertices
+        '''if(self.moving):
+            vertices = [[x, y, z]
+                        for x in (0, self.width)
+                        for y in (0, self.depth)
+                        for z in (0, self.height)]
+            return vertices
+        else:
+            vertices = [[x, y, z + self.z_offset]
+                        for x in (0, self.width)
+                        for y in (0, self.depth)
+                        for z in (0, self.height)]
+            return vertices'''
     
     '''def defineEdges(self):
         edges = [(i, j) 
@@ -56,32 +81,26 @@ class Platform:
         return edges'''
     
     def defineVisibleEdges(self):
-        if(self.moving):
-            visible_edges = [
-                (1,3),
-                (1,5),
-                (2,3),
-                (2,6),
-                (3,7),
-                (4,5),
-                (4,6),
-                (5,7),
-                (6,7)
-            ]
-            return visible_edges
-        else:
-            return
+        visible_edges = [
+            (1,3),
+            (1,5),
+            (2,3),
+            (2,6),
+            (3,7),
+            (4,5),
+            (4,6),
+            (5,7),
+            (6,7)
+        ]
+        return visible_edges
 
     def defineFaces(self):
-        if self.moving:
-            visible_faces = [
-                (1,3,7,5), #top face
-                (2,3,7,6), #left face
-                (4,5,7,6)  #right face
-            ]
-            return visible_faces
-        else:
-            return
+        visible_faces = [
+            (1,3,7,5), #top face
+            (2,3,7,6), #left face
+            (4,5,7,6)  #right face
+        ]
+        return visible_faces
 
     def convertToIsometric(self, x, y ,z):
         iso_x = (x - y) * self.leaningFactor
@@ -120,9 +139,12 @@ class Platform:
 
 class Tower:
     def __init__(self, num, initialColor): #number of platforms, color of the first platform
-        self.platforms = self.definePlatforms(num, initialColor)
+        self.numPlats = num
+        self.initialColor = initialColor
+        self.platforms = self.setupStartingPlatforms()
+        
 
-    def definePlatforms(self, num, initialColor):
+    def setupStartingPlatforms(self):
         platforms = []
 
         def getGradientColor(color, num, index):
@@ -130,17 +152,37 @@ class Tower:
 
             def getRgb(value):
                 offset = value / (num + 1)
-                return max(0, round(value - (offset * (index + 1))))
-
+                value = max(0, int((offset * (index + 1)))) #make it so the value cant be less than 0
+                return min(255, value) #value cant be more than 255
+            
             return (getRgb(r), getRgb(g), getRgb(b))
 
-        for i in range(num):
-            platform = Platform(10, 10, getGradientColor(initialColor, num, i), False)
-            platforms.insert(i, platform)
-
+        for i in range(self.numPlats):
+            z_offset = (i + 1) * PHEIGHT
+            platform = Platform(10, 10, False, z_offset)
+            platform.setup(getGradientColor(self.initialColor, self.numPlats, i))
+            platforms.append(platform)
+        
         return platforms
+    
+    def getTowers(self):
+        return self.platforms
 
-tower = Tower(3, (100, 50, 150))
+    def update(self):
+        for plat in self.platforms:
+            for i in range(len(plat.vertices)):  
+                plat.vertices[i][2] -= 0.1
+            plat.defineVisibleEdges()
+            plat.defineFaces()
+
+    def drawTower(self):
+        
+        for plat in self.platforms:
+            plat.drawFaces()
+
+nPlatforms = 4
+
+tower = Tower(nPlatforms, (255, 0, 0))
 
 clock = pygame.time.Clock()
 running = True
@@ -151,6 +193,8 @@ while running:
             running = False
 
     screen.fill((0, 0, 0))
+
+    tower.drawTower()
 
     pygame.display.flip()
 
