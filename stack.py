3f8c3a910989f92de9af46fcbdcfc2584f8445ad
Pitt <dpittmor@gmail.com>
2025-02-22 19:10:01 +0000

    Update stack.py
    added a color threshold for different colors when making a new gradient

--- stack.py
+++ stack.py
@@ -20,6 +20,7 @@ SBASEWIDTH = 12.5 #starting base's width
 SBASEDEPTH = 12.5 #starting base's height
 MINCVALUE, MAXCVALUE = 50, 205 #MINIMUM AND MAXIMUM COLOR VALUES
 STARTVEL = .3 #starting platform velocity
+COLORTHRESHOLD = 150 #color threshold
 
 ISO_MULTIPLIER = 25
 
@@ -67,10 +68,18 @@ def getGradientColorByGradients(gradients):
 
 def newGradient(startingColor):
     global numPlats
-    numSteps = random.randint(7, 15)
+    numSteps = random.randint(10, 20)
     fromIndex = numPlats
     toIndex = numPlats + numSteps
-    targetColor = (random.randint(MINCVALUE, MAXCVALUE), random.randint(MINCVALUE, MAXCVALUE), random.randint(MINCVALUE, MAXCVALUE))
+
+    def colorDistance(color1, color2):
+        return ((color1[0] - color2[0]) ** 2 + (color1[1] - color2[1]) ** 2 + (color1[2] - color2[2]) ** 2) ** 0.5
+
+
+    while True:
+        targetColor = (random.randint(MINCVALUE, MAXCVALUE), random.randint(MINCVALUE, MAXCVALUE), random.randint(MINCVALUE, MAXCVALUE))
+        if colorDistance(startingColor, targetColor) > COLORTHRESHOLD:
+            break
 
     color = ((fromIndex, toIndex), (startingColor, targetColor), numSteps)
 
